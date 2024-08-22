9960cae4079bf8087a0f937472b399dad7e491a0
Bushati-Klaudio84 <klaudio1@gmail.com>
2024-08-22 17:49:53 +0200

    add

--- MySQL_Advanced/2-fans.sql
+++ MySQL_Advanced/2-fans.sql
@@ -1,13 +1,2 @@
--- Create a table to store the results
-CREATE TEMPORARY TABLE temp_band_fans AS
-SELECT origin, SUM(nb_fans) AS total_fans
-FROM bands
-GROUP BY origin
-ORDER BY total_fans DESC;
-
--- Select the results
-SELECT origin, total_fans AS nb_fans
-FROM temp_band_fans;
-
--- Cleanup: drop the temporary table
-DROP TABLE IF EXISTS temp_band_fans;
+-- Ranks country origins of bands, ordered by the number of (non-unique) fans
+SELECT origin, SUM(fans) AS nb_fans FROM metal_bands GROUP BY origin ORDER BY nb_fans DESC;
