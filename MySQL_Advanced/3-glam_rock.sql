eee1cb92e950cc18ff1bd4a4da577fed129fa8ff
Bushati-Klaudio84 <klaudio1@gmail.com>
2024-08-22 18:06:11 +0200

    add

--- MySQL_Advanced/3-glam_rock.sql
+++ MySQL_Advanced/3-glam_rock.sql
@@ -1,16 +1,11 @@
-
-
--- Assuming the table name is 'bands' and the relevant columns are 'name', 'formed', 'split', and 'style'
-
-SELECT 
-    name AS band_name,
-    CASE
-        WHEN split IS NOT NULL THEN YEAR(split) - YEAR(formed)
-        ELSE YEAR(CURDATE()) - YEAR(formed)
-    END AS lifespan
-FROM 
-    bands
-WHERE 
-    style = 'Glam rock'
-ORDER BY 
+-- Search bands with style Glam rock
+-- Durantion current
+SELECT
+    band_name,
+    IFNULL(split, 2024) - formed AS lifespan
+FROM
+    metal_bands
+WHERE
+    style LIKE '%Glam rock%'
+ORDER BY
     lifespan DESC;
