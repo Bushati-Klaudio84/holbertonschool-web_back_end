d684fc6eb0badadc93f179a86bd72891f8e2acc2
Bushati-Klaudio84 <klaudio1@gmail.com>
2024-09-02 17:14:34 +0200

    11

--- MySQL_Advanced/11-need_meeting.sql
+++ MySQL_Advanced/11-need_meeting.sql
@@ -1,12 +1,10 @@
--- create view need_meeting as
-
+-- Need meeting
+DROP VIEW IF EXISTS need_meeting;
 CREATE VIEW need_meeting AS
-SELECT s.name
-FROM students s
-JOIN scores sc ON s.id = sc.user_id
-WHERE sc.score < 80
-AND (
-    s.last_meeting IS NULL 
-    OR s.last_meeting < DATE_SUB(CURDATE(), INTERVAL 1 MONTH)
-)
-GROUP BY s.id, s.name;
+SELECT name FROM students
+WHERE
+    score < 80 AND
+    (last_meeting IS NULL
+        OR
+    last_meeting < ADDDATE(CURDATE(), interval -1 MONTH)
+    );
